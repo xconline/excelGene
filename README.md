f00d7dbc47740d702ef8e9f927539ebe58161a5a
xc <xccode@qq.com>
2018-08-15 23:58:09 +0800

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-antd demo
+
+-   [eslint prettier 配置](http://web.jobbole.com/94786/)
---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Button, Layout, Menu, Select } from 'antd';
+import './App.less';
+const { Header, Content } = Layout;
+const Option = Select.Option;
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <Header className="header" theme="light">
+                        <Menu
+                            theme="light"
+                            mode="horizontal"
+                            defaultSelectedKeys={['1']}
+                        >
+                            <Menu.Item key="1">资产总表生成</Menu.Item>
+                        </Menu>
+                    </Header>
+                    <Content>
+                        <div className="container">
+                            <div className="svnContainer">
+                                <div className="svnSelectContainer">
+                                    <Select
+                                        mode="multiple"
+                                        style={{ width: '100%' }}
+                                        placeholder="Please select"
+                                        defaultValue={['all']}
+                                    >
+                                        <Option key={1}>xc</Option>
+                                    </Select>
+                                </div>
+                                <div className="svnButContainer">
+                                    <Button>更新</Button>
+                                    <Button>提交</Button>
+                                </div>
+                            </div>
+                            <div className="excelContainer">
+                                <Select
+                                    mode="multiple"
+                                    style={{ width: '100%' }}
+                                    placeholder="Please select"
+                                    defaultValue={['all']}
+                                >
+                                    <Option key={1}>xc</Option>
+                                </Select>
+                                <Button>生成资产总表</Button>
+                            </div>
+                        </div>
+                    </Content>
+                </Layout>
+            </div>
+        );
+    }
+}
+
+export default App;
